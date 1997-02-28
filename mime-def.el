12a12b808fdf02e5bd1b7549975bc6c8aa8334d6
tmorioka <tmorioka>
1997-02-28 04:50:13 +0000

    (call-after-loaded): New function; moved from semi-setup.el.

--- mime-def.el
+++ mime-def.el
@@ -386,6 +386,23 @@ ROT47 will be performed for Japanese text in any case."
        ))
 
 
+;;; @ Other Utility
+;;;
+
+(defun call-after-loaded (module func &optional hook-name)
+  "If MODULE is provided, then FUNC is called.
+Otherwise func is set to MODULE-load-hook.
+If optional argument HOOK-NAME is specified,
+it is used as hook to set."
+  (if (featurep module)
+      (funcall func)
+    (or hook-name
+	(setq hook-name (intern (concat (symbol-name module) "-load-hook")))
+	)
+    (add-hook hook-name func)
+    ))
+
+
 ;;; @ end
 ;;;
 
