073dcc7eea2fe82f7edacbfebe102926410f17a1
morioka <morioka>
1997-09-05 11:38:33 +0000

    (mime-entity-info-encoding): New inline function.

--- mime-parse.el
+++ mime-parse.el
@@ -217,6 +217,9 @@ and return parsed it. [mime-parse.el]"
 (defsubst mime-entity-info-parameters (entity-info)
   (mime::content-info/parameters entity-info))
 
+(defsubst mime-entity-info-encoding (entity-info)
+  (mime::content-info/encoding entity-info))
+
 (defun mime-parse-multipart (boundary primtype subtype params encoding rcnum)
   (goto-char (point-min))
   (let* ((dash-boundary   (concat "--" boundary))
