a1cadef3afae859e09e1d000c5d6350c9ebac775
morioka <morioka>
1998-03-15 13:28:14 +0000

    (mime-root-entity-p): New function.

--- mime-parse.el
+++ mime-parse.el
@@ -273,6 +273,14 @@ mime-{parse|read}-Content-Type."
       )))
 
 
+;;; @ utilities
+;;;
+
+(defsubst mime-root-entity-p (entity)
+  "Return t if ENTITY is root-entity (message)."
+  (null (mime-entity-node-id entity)))
+
+
 ;;; @ end
 ;;;
 
