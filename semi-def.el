4495315035dcdbed7ae1b2cb8159374de30c6b6f
morioka <morioka>
1998-04-22 13:04:02 +0000

    (mime-module-version): update.

--- semi-def.el
+++ semi-def.el
@@ -26,7 +26,7 @@
 
 (require 'emu)
 
-(defconst mime-module-version '("SEMI" "Tsutsuishi" 1 3 1)
+(defconst mime-module-version '("SEMI" "N.DNr" 1 3 2)
   "Implementation name, version name and numbers of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
