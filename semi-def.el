ed950a0a242f982b5b30fa2e86611cf8a6624305
morioka <morioka>
1998-05-05 19:45:47 +0000

    update.

--- semi-def.el
+++ semi-def.el
@@ -29,7 +29,7 @@
 (eval-when-compile (require 'cl))
 
 
-(defconst mime-module-version '("SEMI" "Itoigawa" 1 4 0)
+(defconst mime-module-version '("SEMI" ".DNRmi" 1 4 1)
   "Implementation name, version name and numbers of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
