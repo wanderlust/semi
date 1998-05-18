bed400f2385b1087f91aee0d56e99e4535fab0e5
morioka <morioka>
1998-05-18 16:18:05 +0000

    Sync up with remi-1_4_0_9.

--- semi-def.el
+++ semi-def.el
@@ -29,7 +29,7 @@
 (eval-when-compile (require 'cl))
 
 
-(defconst mime-module-version '("WEMI" "Hiratsuka" 1 4 3)
+(defconst mime-module-version '("WEMI" ",DR(Biso" 1 4 4)
   "Implementation name, version name and numbers of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
