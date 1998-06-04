5ef2d74f1b1d86a7b336a83c896a22db671d1e43
morioka <morioka>
1998-06-04 08:55:38 +0000

    update.

--- semi-def.el
+++ semi-def.el
@@ -29,7 +29,7 @@
 (eval-when-compile (require 'cl))
 
 
-(defconst mime-module-version '("SEMI" "Nishi-Ny,D~(Bzen" 1 5 0)
+(defconst mime-module-version '("SEMI" "Ikuji" 1 5 1)
   "Implementation name, version name and numbers of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
