d9a285e61d7add50ffe42fe6112e506eff8f87cd
morioka <morioka>
1998-10-16 02:41:07 +0000

    (mime-view-caesar): Use `mime-insert-text-content' instead of `mime-text-insert-decoded-body'.

--- mime-play.el
+++ mime-play.el
@@ -705,7 +705,7 @@ It is registered to variable `mime-preview-quitting-method-alist'."
       )
     (setq buffer-read-only nil)
     (erase-buffer)
-    (mime-text-insert-decoded-body entity)
+    (mime-insert-text-content entity)
     (mule-caesar-region (point-min) (point-max))
     (set-buffer-modified-p nil)
     (set-buffer mother)
