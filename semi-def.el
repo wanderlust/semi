a380901df62ca240ee96c9dadfa4b7fb814feea3
morioka <morioka>
1998-10-24 13:36:39 +0000

    (mime-user-interface-product): update.

--- semi-def.el
+++ semi-def.el
@@ -30,7 +30,7 @@
 
 (require 'custom)
 
-(defconst mime-user-interface-product ["SEMI" (1 10 0) "Tsubata"]
+(defconst mime-user-interface-product ["SEMI" (1 10 1) "Morimoto"]
   "Product name, version number and code name of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
