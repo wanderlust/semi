57fa064cb61187f4a421432878c67fb00f5a1375
yamaoka <yamaoka>
1998-10-27 10:27:09 +0000

    Sync up with semi-1_10.

--- semi-def.el
+++ semi-def.el
@@ -30,7 +30,7 @@
 
 (require 'custom)
 
-(defconst mime-user-interface-product '["WEMI" (1 10 1) "Kambara"]
+(defconst mime-user-interface-product '["WEMI" (1 10 2) "Yui"]
   "Implementation name, version name and numbers of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
