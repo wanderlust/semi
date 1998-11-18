3d6a2134194de366d54f0d61f6c2405e7fac5392
yamaoka <yamaoka>
1998-11-18 23:06:07 +0000

    Sync up with the latest semi-1_11.

--- mime-view.el
+++ mime-view.el
@@ -1149,7 +1149,7 @@ It calls following-method selected from variable
 		    ))
 	      (setq rest (cdr rest))
 	      ))
-	  (eword-decode-header)
+	  (mime-decode-header-in-buffer)
 	  )
 	(let ((f (cdr (assq mode mime-preview-following-method-alist))))
 	  (if (functionp f)
