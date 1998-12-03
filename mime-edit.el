a4227473eba9fae78ddda6a3f77053bb78d262b2
yamaoka <yamaoka>
1998-12-03 08:44:07 +0000

    * mime-edit.el (mime-edit-user-agent-value): Include `xemacs-betaname'.

--- mime-edit.el
+++ mime-edit.el
@@ -633,6 +633,12 @@ If it is not specified for a major-mode,
 			  (concat "/"
 				  (substring emacs-version 0
 					     (match-beginning 0))
+				  (if (and (boundp 'xemacs-betaname)
+					   ;; It does not exist in XEmacs
+					   ;; versions prior to 20.3.
+					   xemacs-betaname)
+				      (concat " " xemacs-betaname)
+				    "")
 				  " (" xemacs-codename ") ("
 				  system-configuration ")")
 			" (" emacs-version ")"))
