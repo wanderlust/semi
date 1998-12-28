df061db8c8dc5ab6f54c4dcd80bb75bb747048b9
morioka <morioka>
1998-12-28 06:07:45 +0000

    (mime-user-interface-product): update.

--- semi-def.el
+++ semi-def.el
@@ -30,7 +30,7 @@
 
 (require 'custom)
 
-(defconst mime-user-interface-product ["SEMI" (1 12 0) "Nishi-Kanazawa"]
+(defconst mime-user-interface-product ["SEMI" (1 12 1) "[JR] Nonoichi"]
   "Product name, version number and code name of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
