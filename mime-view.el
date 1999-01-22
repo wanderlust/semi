b1dd2d2458a088c7f56de394aa5f632fd1c59250
morioka <morioka>
1999-01-22 11:33:46 +0000

    (mime-view-version): Don't use `eval-when-compile' to avoid to break product-code-name.

--- mime-view.el
+++ mime-view.el
@@ -39,11 +39,10 @@
 ;;;
 
 (defconst mime-view-version
-  (eval-when-compile
-    (concat (mime-product-name mime-user-interface-product) " MIME-View "
-	    (mapconcat #'number-to-string
-		       (mime-product-version mime-user-interface-product) ".")
-	    " (" (mime-product-code-name mime-user-interface-product) ")")))
+  (concat (mime-product-name mime-user-interface-product) " MIME-View "
+	  (mapconcat #'number-to-string
+		     (mime-product-version mime-user-interface-product) ".")
+	  " (" (mime-product-code-name mime-user-interface-product) ")"))
 
 
 ;;; @ variables
