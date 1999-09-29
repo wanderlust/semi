944214aca1629f30db15e290bcc056d3a368b856
yamaoka <yamaoka>
1999-09-29 22:15:24 +0000

    Sync up with semi-1_13.

--- mime-edit.el
+++ mime-edit.el
@@ -193,7 +193,7 @@ To insert a signature file automatically, call the function
      ("css") ; rfc2318
      ("xml") ; rfc2376
      ("x-latex")
-     ("x-rot13-47-48")
+     ;; ("x-rot13-47-48")
      )
     ("message"
      ("external-body"
