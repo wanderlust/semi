f0a542b3b4c5dde1c5d165e567b2a1eeebe7be19
yoichi <yoichi>
2002-11-05 13:52:21 +0000

    * mime-view.el (mime-preview-find-boundary-info): Change the name of the argument from get-mother to with-children along its effect.

--- mime-view.el
+++ mime-view.el
@@ -1472,9 +1472,9 @@ button-2	Move to point under the mouse cursor
 ;;; @@ utility
 ;;;
 
-(defun mime-preview-find-boundary-info (&optional get-mother)
+(defun mime-preview-find-boundary-info (&optional with-children)
   "Return boundary information of current part.
-If GET-MOTHER, refer boundary surrounding current part and its branches."
+If WITH-CHILDREN, refer boundary surrounding current part and its branches."
   (let (entity
 	p-beg p-end
 	entity-node-id len)
@@ -1503,7 +1503,7 @@ If GET-MOTHER, refer boundary surrounding current part and its branches."
 	  ((null entity-node-id)
 	   (setq p-end (point-max))
 	   )
-	  (get-mother
+	  (with-children
 	   (save-excursion
 	     (catch 'tag
 	       (let (e i)
