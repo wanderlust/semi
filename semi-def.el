e29d8816898339474e8392e135e5c2cb60882965
tomo <tomo>
2002-11-07 11:38:34 +0000

    (mime-user-interface-product): Update to 1.14.5.

--- semi-def.el
+++ semi-def.el
@@ -28,7 +28,7 @@
 
 (require 'custom)
 
-(defconst mime-user-interface-product ["SEMI" (1 14 4) "Hosorogi"]
+(defconst mime-user-interface-product ["SEMI" (1 14 5) "Awara-Onsen"]
   "Product name, version number and code name of MIME-kernel package.")
 
 (autoload 'mule-caesar-region "mule-caesar"
