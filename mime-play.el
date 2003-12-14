b7d0a5b67213eb508bcf6f16bc99e01182533b1b
yoichi <yoichi>
2003-12-14 13:31:19 +0000

    merge from semi-1_14 branch: * mime-play.el (mime-activate-mailcap-method): Don't use `binary-to-text-funcall'. (Thanks to TANAKA Shingo)

--- mime-play.el
+++ mime-play.el
@@ -145,11 +145,10 @@ specified, play as it.  Default MODE is \"play\"."
 	   (let ((command
 		  (mime-format-mailcap-command
 		   method
-		   (cons (cons 'filename name) situation))))
-	     (binary-to-text-funcall
-	      mime-play-messages-coding-system
-	      #'start-process command mime-echo-buffer-name
-	      shell-file-name shell-command-switch command))))
+		   (cons (cons 'filename name) situation)))
+		 (coding-system-for-read mime-play-messages-coding-system))
+	     (start-process command mime-echo-buffer-name
+			    shell-file-name shell-command-switch command))))
       (set-alist 'mime-mailcap-method-filename-alist process name)
       (set-process-sentinel process 'mime-mailcap-method-sentinel))))
 
